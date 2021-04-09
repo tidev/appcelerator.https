ffb152050eeced8ab4955281a31e625d01a2a73d
Christopher Williams <chris.a.williams@gmail.com>
2021-04-09 16:09:03 -0400

    style(ios): fix formatting

--- iphone/Classes/AppceleratorHttpsModuleAssets.m
+++ iphone/Classes/AppceleratorHttpsModuleAssets.m
@@ -3,20 +3,18 @@
  */
 #import "AppceleratorHttpsModuleAssets.h"
 
-extern NSData* filterDataInRange(NSData* thedata, NSRange range);
+extern NSData *filterDataInRange(NSData *thedata, NSRange range);
 
 @implementation AppceleratorHttpsModuleAssets
 
 - (NSData *)moduleAsset
 {
-  
 
   return nil;
 }
 
 - (NSData *)resolveModuleAsset:(NSString *)path
 {
-  
 
   return nil;
 }
